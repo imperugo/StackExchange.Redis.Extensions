a2b8bc0946e931281d40598ff0b003dc3bd48598
Eugene Krapivin <nocgod@gmail.com>
2016-08-28 12:11:39 +0300

    removed `exe` extensions from NuGet.bat/sh files
    trying to fix appveyor build file

--- NuGetPack.sh
+++ NuGetPack.sh
@@ -1,5 +1,5 @@
-dotnet.exe pack .\src\StackExchange.Redis.Extensions.Core\project.json -c Release -o .\nuget
-dotnet.exe pack .\src\Serializers\StackExchange.Redis.Extensions.Jil\project.json -c Release -o .\nuget 
-dotnet.exe pack .\src\Serializers\StackExchange.Redis.Extensions.Newtonsoft\project.json -c Release -o .\nuget 
-dotnet.exe pack .\src\Serializers\StackExchange.Redis.Extensions.MsgPack\project.json -c Release -o .\nuget 
-dotnet.exe pack .\src\Serializers\StackExchange.Redis.Extensions.Protobuf\project.json -c Release -o .\nuget 
+dotnet pack .\src\StackExchange.Redis.Extensions.Core\project.json -c Release -o .\nuget
+dotnet pack .\src\Serializers\StackExchange.Redis.Extensions.Jil\project.json -c Release -o .\nuget 
+dotnet pack .\src\Serializers\StackExchange.Redis.Extensions.Newtonsoft\project.json -c Release -o .\nuget 
+dotnet pack .\src\Serializers\StackExchange.Redis.Extensions.MsgPack\project.json -c Release -o .\nuget 
+dotnet pack .\src\Serializers\StackExchange.Redis.Extensions.Protobuf\project.json -c Release -o .\nuget 
