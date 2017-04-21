ffb70c97820386451e0c3d16b1ccf1ba1f9ecd7b
Rush Frisby <rush@rushfrisby.com>
2017-04-20 20:07:20 -0400

    Add BinaryFormatter as a new serializer option

---
+++ src/StackExchange.Redis.Extensions.Binary/BinarySerializer.cs
@@ -0,0 +1,52 @@
+﻿using StackExchange.Redis.Extensions.Core;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+namespace StackExchange.Redis.Extensions.Binary
+{
+    public class BinarySerializer : ISerializer
+    {
+        private readonly BinaryFormatter _binaryFormatter = new BinaryFormatter();
+
+        public object Deserialize(byte[] serializedObject)
+        {
+            using (var ms = new MemoryStream(serializedObject))
+            {
+                return _binaryFormatter.Deserialize(ms);
+            }
+        }
+
+        public T Deserialize<T>(byte[] serializedObject)
+        {
+            using (var ms = new MemoryStream(serializedObject))
+            {
+                return (T)_binaryFormatter.Deserialize(ms);
+            }
+        }
+
+        public Task<object> DeserializeAsync(byte[] serializedObject)
+        {
+            return Task.Factory.StartNew(() => Deserialize(serializedObject));
+        }
+
+        public Task<T> DeserializeAsync<T>(byte[] serializedObject)
+        {
+            return Task.Factory.StartNew(() => Deserialize<T>(serializedObject));
+        }
+
+        public byte[] Serialize(object item)
+        {
+            using (var ms = new MemoryStream())
+            {
+                _binaryFormatter.Serialize(ms, item);
+                return ms.ToArray();
+            }
+        }
+
+        public Task<byte[]> SerializeAsync(object item)
+        {
+            return Task.Factory.StartNew(() => Serialize(item));
+        }
+    }
+}
