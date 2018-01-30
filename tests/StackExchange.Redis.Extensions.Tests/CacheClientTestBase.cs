4577106118ccbf2033e4c03fdc4f04d48a651715
imperugo <imperugo@gmail.com>
2018-01-30 15:34:55 +0100

    removed useless configuration

--- tests/StackExchange.Redis.Extensions.Tests/CacheClientTestBase.cs
+++ tests/StackExchange.Redis.Extensions.Tests/CacheClientTestBase.cs
@@ -35,29 +35,6 @@ namespace StackExchange.Redis.Extensions.Tests
 
             Sut = new StackExchangeRedisCacheClient(mux, Serializer);
 			Db = Sut.Database;
-
-			var redisConfiguration = new RedisConfiguration()
-			{
-				AbortOnConnectFail = true,
-				KeyPrefix = "_my_key_prefix_",
-				Hosts = new RedisHost[]
-				{
-					new RedisHost(){Host = "192.168.0.10", Port = 6379},
-					new RedisHost(){Host = "192.168.0.11",  Port =6379},
-					new RedisHost(){Host = "192.168.0.12",  Port =6379}
-				},
-				AllowAdmin = true,
-				ConnectTimeout = 3000,
-				Database = 0,
-				Ssl = true,
-				Password = "my_super_secret_password",
-				ServerEnumerationStrategy = new ServerEnumerationStrategy()
-				{
-					Mode = ServerEnumerationStrategy.ModeOptions.All,
-					TargetRole = ServerEnumerationStrategy.TargetRoleOptions.Any,
-					UnreachableServerAction = ServerEnumerationStrategy.UnreachableServerActionOptions.Throw
-				}
-			};
 		}
 
 		public void Dispose()
