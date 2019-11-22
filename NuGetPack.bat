d4f70a3d1fb43d83141cfcba9a5896d9efaa7ba6
imperugo <imperugo@gmail.com>
2019-11-22 14:55:43 +0100

    fixed wrong package id

--- NuGetPack.bat
+++ NuGetPack.bat
@@ -1,19 +1,19 @@
-dotnet pack src\StackExchange.Redis.Extensions.Core\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.Core\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.Binary\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.Binary\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.Jil\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.Jil\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.MsgPack\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.MsgPack\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.Newtonsoft\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.Newtonsoft\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.Protobuf\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.Protobuf\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.Utf8Json\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.Utf8Json\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.System.Text.Json\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.System.Text.Json\ -o .\packages\ -c Release
 
-dotnet pack src\StackExchange.Redis.Extensions.AspNetCore\ -o ..\..\packages\ -c Release
+dotnet pack src\StackExchange.Redis.Extensions.AspNetCore\ -o .\packages\ -c Release
 
 .\.nuget\NuGet.exe pack .\src\StackExchange.Redis.Extensions.LegacyConfiguration\StackExchange.Redis.Extensions.LegacyConfiguration.csproj -Prop Configuration=Release -OutputDirectory .\packages\
