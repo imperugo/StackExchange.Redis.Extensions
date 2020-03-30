916927f2bc9baf8b1368ad6f2b8232274f730df7
GY101 <gaoyong101@outlook.com>
2020-03-30 22:18:52 +0800

    add poolSize property add DefaultValue

--- src/StackExchange.Redis.Extensions.LegacyConfiguration/RedisCachingSectionHandler.cs
+++ src/StackExchange.Redis.Extensions.LegacyConfiguration/RedisCachingSectionHandler.cs
@@ -7,133 +7,79 @@ using RedisHost = StackExchange.Redis.Extensions.LegacyConfiguration.Configurati
 
 namespace StackExchange.Redis.Extensions.LegacyConfiguration
 {
-	/// <summary>
-	/// The implementation of <see cref="IRedisCachingConfiguration"/>
-	/// </summary>
-	public class RedisCachingSectionHandler : ConfigurationSection, IRedisCachingConfiguration
-	{
-		/// <summary>
-		/// The host of Redis Server
-		/// </summary>
-		/// <value>
-		/// The ip or name
-		/// </value>
-		[ConfigurationProperty("hosts")]
-		public RedisHostCollection RedisHosts
-			=> this["hosts"] as RedisHostCollection;
+    /// <summary>
+    /// The implementation of <see cref="IRedisCachingConfiguration"/>
+    /// </summary>
+    public class RedisCachingSectionHandler : ConfigurationSection, IRedisCachingConfiguration
+    {
+        /// <summary>
+        /// The host of Redis Server
+        /// </summary>
+        /// <value>
+        /// The ip or name
+        /// </value>
+        [ConfigurationProperty("hosts")]
+        public RedisHostCollection RedisHosts
+            => this["hosts"] as RedisHostCollection;
 
-		/// <summary>
-		/// The strategy to use when executing server wide commands
-		/// </summary>
-		[ConfigurationProperty("serverEnumerationStrategy")]
-		public ServerEnumerationStrategyConfiguration ServerEnumerationStrategy
-			=> this["serverEnumerationStrategy"] as ServerEnumerationStrategyConfiguration;
-		
+        /// <summary>
+        /// The strategy to use when executing server wide commands
+        /// </summary>
+        [ConfigurationProperty("serverEnumerationStrategy")]
+        public ServerEnumerationStrategyConfiguration ServerEnumerationStrategy
+            => this["serverEnumerationStrategy"] as ServerEnumerationStrategyConfiguration;
 
-		/// <summary>
-		/// Specify if the connection can use Admin commands like flush database
-		/// </summary>
-		/// <value>
-		///   <c>true</c> if can use admin commands; otherwise, <c>false</c>.
-		/// </value>
-		[ConfigurationProperty("allowAdmin")]
-		public bool AllowAdmin
-		{
-			get
-			{
-			    var value = this["allowAdmin"]?.ToString();
 
-                bool result;
-                return !string.IsNullOrEmpty(value) && bool.TryParse(value, out result) && result;
-			}
-		}
+        /// <summary>
+        /// Specify if the connection can use Admin commands like flush database
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if can use admin commands; otherwise, <c>false</c>.
+        /// </value>
+        [ConfigurationProperty("allowAdmin")]
+        public bool AllowAdmin { get => (bool)base["allowAdmin"]; }
 
-		/// <summary>
-		/// Specify if the connection is a secure connection or not.
-		/// </summary>
-		/// <value>
-		///   <c>true</c> if is secure; otherwise, <c>false</c>.
-		/// </value>
-		[ConfigurationProperty("ssl")]
-		public bool Ssl
-		{
-			get
-			{
-			    var value = this["ssl"]?.ToString();
+        /// <summary>
+        /// Specify if the connection is a secure connection or not.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if is secure; otherwise, <c>false</c>.
+        /// </value>
+        [ConfigurationProperty("ssl")]
+        public bool Ssl { get => (bool)base["ssl"]; }
 
-                bool result;
-                return !string.IsNullOrWhiteSpace(value) && bool.TryParse(value, out result) && result;
-			}
-		}
+        /// <summary>
+        /// The connection timeout
+        /// </summary>
+        [ConfigurationProperty("connectTimeout", DefaultValue = 5000)]
+        public int ConnectTimeout { get=> (int)base["connectTimeout"]; }
 
-		/// <summary>
-		/// The connection timeout
-		/// </summary>
-		[ConfigurationProperty("connectTimeout")]
-		public int ConnectTimeout
-		{
-			get
-			{
-			    var value = this["connectTimeout"]?.ToString();
+        /// <summary>
+        /// Time (ms) to allow for synchronous operations
+        /// </summary>
+        [ConfigurationProperty("syncTimeout", DefaultValue = 1000)]
+        public int SyncTimeout { get => (int)base["syncTimeout"]; }
 
-                int result;
-			    return !string.IsNullOrWhiteSpace(value) && int.TryParse(value, out result) ? result : 5000;
-			}
-		}
+        /// <summary>
+        /// If true, Connect will not create a connection while no servers are available
+        /// </summary>
+        [ConfigurationProperty("abortOnConnectFail")]
+        public bool AbortOnConnectFail { get => (bool)base["abortOnConnectFail"]; }
 
-		/// <summary>
-		/// Time (ms) to allow for synchronous operations
-		/// </summary>
-		[ConfigurationProperty("syncTimeout")]
-		public int SyncTimeout
-		{
-			get
-			{
-			    var value = this["syncTimeout"]?.ToString();
+        /// <summary>
+        /// Database Id
+        /// </summary>
+        /// <value>
+        /// The database id, the default value is 0
+        /// </value>
+        [ConfigurationProperty("database", DefaultValue = 0)]
+        public int Database { get => (int)base["database"]; }
 
-				int result;
-			    return !string.IsNullOrWhiteSpace(value) && int.TryParse(value, out result) ? result : 1000;
-			}
-		}
-
-		/// <summary>
-		/// If true, Connect will not create a connection while no servers are available
-		/// </summary>
-		[ConfigurationProperty("abortOnConnectFail")]
-		public bool AbortOnConnectFail
-		{
-			get
-			{
-			    var value = this["abortOnConnectFail"]?.ToString();
-
-                bool result;
-			    return !string.IsNullOrWhiteSpace(value) && bool.TryParse(value, out result) && result;
-			}
-		}
-
-		/// <summary>
-		/// Database Id
-		/// </summary>
-		/// <value>
-		/// The database id, the default value is 0
-		/// </value>
-		[ConfigurationProperty("database")]
-		public int Database
-		{
-			get
-			{
-			    var value = this["database"]?.ToString();
-
-			    int result;
-			    return !string.IsNullOrWhiteSpace(value) && int.TryParse(value, out result) ? result : 0;
-			}
-		}
-
-		/// <summary>
-		/// The password or access key
-		/// </summary>
-		[ConfigurationProperty("password", IsRequired = false)]
-		public string Password => this["password"] as string;
+        /// <summary>
+        /// The password or access key
+        /// </summary>
+        [ConfigurationProperty("password", IsRequired = false)]
+        public string Password => this["password"] as string;
 
         /// <summary>
         /// The key separation prefix used for all cache entries
@@ -142,48 +88,55 @@ namespace StackExchange.Redis.Extensions.LegacyConfiguration
         public string KeyPrefix => this["keyprefix"] as string;
 
         /// <summary>
+        /// Redis connections pool size
+        /// </summary>
+        [ConfigurationProperty("poolSize", DefaultValue = 5)]
+        public int PoolSize { get => (int)base["poolSize"]; }
+
+        /// <summary>
         /// Gets the configuration.
         /// </summary>
         /// <returns></returns>
         public static RedisConfiguration GetConfig()
         {
-	        var cfg = ConfigurationManager.GetSection("redisCacheClient") as RedisCachingSectionHandler;
+            var cfg = ConfigurationManager.GetSection("redisCacheClient") as RedisCachingSectionHandler;
 
-	        if (cfg == null)
-	        {
-		        throw new ConfigurationErrorsException("Unable to locate <redisCacheClient> section into your configuration file. Take a look https://github.com/imperugo/StackExchange.Redis.Extensions");
-	        }
+            if (cfg == null)
+            {
+                throw new ConfigurationErrorsException("Unable to locate <redisCacheClient> section into your configuration file. Take a look https://github.com/imperugo/StackExchange.Redis.Extensions");
+            }
 
-	        RedisConfiguration result = new RedisConfiguration();
-	        result.AbortOnConnectFail = cfg.AbortOnConnectFail;
-	        result.AllowAdmin = cfg.AllowAdmin;
-	        result.ConnectTimeout = cfg.ConnectTimeout;
-	        result.SyncTimeout = cfg.SyncTimeout;
-	        result.Database = cfg.Database;
-	        result.KeyPrefix = cfg.KeyPrefix;
-	        result.Password = cfg.Password;
-	        result.Ssl = cfg.Ssl;
+            RedisConfiguration result = new RedisConfiguration();
+            result.AbortOnConnectFail = cfg.AbortOnConnectFail;
+            result.AllowAdmin = cfg.AllowAdmin;
+            result.ConnectTimeout = cfg.ConnectTimeout;
+            result.SyncTimeout = cfg.SyncTimeout;
+            result.Database = cfg.Database;
+            result.KeyPrefix = cfg.KeyPrefix;
+            result.Password = cfg.Password;
+            result.Ssl = cfg.Ssl;
+            result.PoolSize = cfg.PoolSize;
 
-			List<Core.Configuration.RedisHost> hosts = new List<Core.Configuration.RedisHost>();
+            List<Core.Configuration.RedisHost> hosts = new List<Core.Configuration.RedisHost>();
 
-	        foreach (RedisHost host in cfg.RedisHosts)
-	        {
-		        hosts.Add(new Core.Configuration.RedisHost()
-		        {
-					Host = host.Host,
-					Port = host.CachePort
-		        });
-	        }
+            foreach (RedisHost host in cfg.RedisHosts)
+            {
+                hosts.Add(new Core.Configuration.RedisHost()
+                {
+                    Host = host.Host,
+                    Port = host.CachePort
+                });
+            }
 
-			result.Hosts = hosts.ToArray();
-	        result.ServerEnumerationStrategy = new ServerEnumerationStrategy()
-	        {
-		        UnreachableServerAction = cfg.ServerEnumerationStrategy.UnreachableServerAction,
-		        TargetRole = cfg.ServerEnumerationStrategy.TargetRole,
-		        Mode = cfg.ServerEnumerationStrategy.Mode
-	        };
+            result.Hosts = hosts.ToArray();
+            result.ServerEnumerationStrategy = new ServerEnumerationStrategy()
+            {
+                UnreachableServerAction = cfg.ServerEnumerationStrategy.UnreachableServerAction,
+                TargetRole = cfg.ServerEnumerationStrategy.TargetRole,
+                Mode = cfg.ServerEnumerationStrategy.Mode
+            };
 
-	        return result;
-        } 
-	}
+            return result;
+        }
+    }
 }
--- tests/StackExchange.Redis.Extensions.LegacyConfiguration.Tests/RedisCachingSectionHandlerTests.cs
+++ tests/StackExchange.Redis.Extensions.LegacyConfiguration.Tests/RedisCachingSectionHandlerTests.cs
@@ -8,42 +8,44 @@ using Xunit;
 
 namespace StackExchange.Redis.Extensions.LegacyConfiguration.Tests
 {
-	public class RedisCachingSectionHandlerTests : IDisposable
-	{
-		private RedisCachingSectionHandler sut;
+    public class RedisCachingSectionHandlerTests : IDisposable
+    {
+        private RedisCachingSectionHandler sut;
 
-		public RedisCachingSectionHandlerTests()
-		{
-			sut = new RedisCachingSectionHandler();
-		}
+        public RedisCachingSectionHandlerTests()
+        {
+            sut = new RedisCachingSectionHandler();
+        }
 
-		public void Dispose()
-		{
-			(sut as IDisposable)?.Dispose();
-			sut = null;
-		}
+        public void Dispose()
+        {
+            (sut as IDisposable)?.Dispose();
+            sut = null;
+        }
 
-		[Fact]
-		public void ReadingFromConfigurationFile_ShouldReturnValidValues()
-		{
-			//NOTE: This isn't a good test, need to find a way to inject a config file in memory (no idea how to do that)
-			var cfg = RedisCachingSectionHandler.GetConfig();
+        [Fact]
+        public void ReadingFromConfigurationFile_ShouldReturnValidValues()
+        {
+            //NOTE: This isn't a good test, need to find a way to inject a config file in memory (no idea how to do that)
+            var cfg = RedisCachingSectionHandler.GetConfig();
 
-			Assert.NotNull(cfg);
-			Assert.NotNull(cfg.ServerEnumerationStrategy);
-			Assert.NotNull(cfg.Hosts);
-			Assert.True(cfg.AllowAdmin);
-			Assert.False(cfg.Ssl);
-			Assert.Equal(3000, cfg.ConnectTimeout);
-			Assert.Equal(24, cfg.Database);
+            Assert.NotNull(cfg);
+            Assert.NotNull(cfg.ServerEnumerationStrategy);
+            Assert.NotNull(cfg.Hosts);
+            Assert.True(cfg.AllowAdmin);
+            Assert.False(cfg.Ssl);
+            Assert.Equal(3000, cfg.ConnectTimeout);
+            Assert.Equal(1000, cfg.SyncTimeout);
+            Assert.Equal(24, cfg.Database);
+            Assert.Equal(10, cfg.PoolSize);
 
-			Assert.Equal(ServerEnumerationStrategy.ModeOptions.Single,cfg.ServerEnumerationStrategy.Mode);
-			Assert.Equal(ServerEnumerationStrategy.TargetRoleOptions.PreferSlave,cfg.ServerEnumerationStrategy.TargetRole);
-			Assert.Equal(ServerEnumerationStrategy.UnreachableServerActionOptions.IgnoreIfOtherAvailable,cfg.ServerEnumerationStrategy.UnreachableServerAction);
+            Assert.Equal(ServerEnumerationStrategy.ModeOptions.Single, cfg.ServerEnumerationStrategy.Mode);
+            Assert.Equal(ServerEnumerationStrategy.TargetRoleOptions.PreferSlave, cfg.ServerEnumerationStrategy.TargetRole);
+            Assert.Equal(ServerEnumerationStrategy.UnreachableServerActionOptions.IgnoreIfOtherAvailable, cfg.ServerEnumerationStrategy.UnreachableServerAction);
 
-			Assert.Single(cfg.Hosts);
-			Assert.Equal("127.0.0.1",cfg.Hosts.First().Host);
-			Assert.Equal(6379,cfg.Hosts.First().Port);
-		}
-	}
+            Assert.Single(cfg.Hosts);
+            Assert.Equal("127.0.0.1", cfg.Hosts.First().Host);
+            Assert.Equal(6379, cfg.Hosts.First().Port);
+        }
+    }
 }
