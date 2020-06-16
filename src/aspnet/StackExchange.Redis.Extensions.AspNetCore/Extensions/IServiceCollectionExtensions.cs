9d1704ab6c9739b5d91e9ded36878774eb0567ef
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2020-06-16 12:24:49 +0300

    Add redis configuration factory support

--- src/aspnet/StackExchange.Redis.Extensions.AspNetCore/Extensions/IServiceCollectionExtensions.cs
+++ src/aspnet/StackExchange.Redis.Extensions.AspNetCore/Extensions/IServiceCollectionExtensions.cs
@@ -17,7 +17,23 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="services">The service collection.</param>
         /// <param name="redisConfiguration">The redis configration.</param>
         /// <typeparam name="T">The typof of serializer. <see cref="ISerializer" />.</typeparam>
-        public static IServiceCollection AddStackExchangeRedisExtensions<T>(this IServiceCollection services, RedisConfiguration redisConfiguration)
+        public static IServiceCollection AddStackExchangeRedisExtensions<T>(
+            this IServiceCollection services,
+            RedisConfiguration redisConfiguration)
+            where T : class, ISerializer, new()
+        {
+            return services.AddStackExchangeRedisExtensions<T>(sp => redisConfiguration);
+        }
+        
+        /// <summary>
+        /// Add StackExchange.Redis with its serialization provider.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="redisConfigurationFactory">The redis configration factory.</param>
+        /// <typeparam name="T">The typof of serializer. <see cref="ISerializer" />.</typeparam>
+        public static IServiceCollection AddStackExchangeRedisExtensions<T>(
+            this IServiceCollection services,
+            Func<IServiceProvider, RedisConfiguration> redisConfigurationFactory)
             where T : class, ISerializer, new()
         {
             services.AddSingleton<IRedisCacheClient, RedisCacheClient>();
@@ -29,7 +45,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 return provider.GetRequiredService<IRedisCacheClient>().GetDbFromConfiguration();
             });
 
-            services.AddSingleton(redisConfiguration);
+            services.AddSingleton(redisConfigurationFactory);
 
             return services;
         }
