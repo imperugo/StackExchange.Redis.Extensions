a7ac3de80d41c3100fdf80fd7d875b5fa166653a
Mariachi1231 <onyxlexa@gmail.com>
2020-11-09 10:11:04 +0200

    - Add posibility to configure StateAwareConnection (#345)
    Co-authored-by: Oleksandr Tolstikov <Oleksandr.Tolstikov@selerant.com>

---
+++ src/core/StackExchange.Redis.Extensions.Core/Abstractions/IStateAwareConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StackExchange.Redis.Extensions.Core.Abstractions
+{
+    /// <summary>
+    /// Interface in order to wrap <see cref="IConnectionMultiplexer"/> for pooling
+    /// </summary>
+    public interface IStateAwareConnection : IDisposable
+    {
+        /// <summary>
+        /// Gets wrapped <see cref="IConnectionMultiplexer"/>
+        /// </summary>
+        IConnectionMultiplexer Connection { get; }
+
+        /// <summary>
+        /// Indicates whether connection is established
+        /// </summary>
+        bool IsConnected();
+
+        /// <summary>
+        /// Indicates the total number of outstanding items against this server
+        /// </summary>
+        long TotalOutstanding();
+    }
+}
