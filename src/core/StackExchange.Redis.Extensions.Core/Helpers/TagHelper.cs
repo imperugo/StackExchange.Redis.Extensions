b35c8881a551bc1793b5c52cfa5e883fd6f4b710
Dmytro Hridin <dmytro.hridin@gmail.com>
2021-04-18 19:04:03 +0300

    [V7] Tags support (#317)
    * Added option to use connection string instead of all property configuration
    
    * Added v7 branch to github actions
    
    * Implement basic operations with tags (Add, HashAdd, SetAdd)
    
    * Implemet method for get values by tag
    
    * Rollback changes related to hashes and sets with tags
    
    * rollback taghelper
    
    * add summaries
    
    * change TagHelper from public to internal
    
    * add InternalsVisibleTo
    
    * add tests for removebytag
    
    * Remove TagHashValue
    
    * updated packages
    
    * Updated the version
    
    Co-authored-by: Ugo Lattanzi <imperugo@gmail.com>

---
+++ src/core/StackExchange.Redis.Extensions.Core/Helpers/TagHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace StackExchange.Redis.Extensions.Core.Helpers
+{
+    /// <summary>
+    /// Helper for generating ta key
+    /// </summary>
+    public static class TagHelper
+    {
+        /// <summary>
+        ///     Generate key associated with tag
+        /// </summary>
+        /// <param name="tag">Tag</param>
+        /// <returns>Return key associated with tag</returns>
+        public static string GenerateTagKey(string tag) => "tag:" + tag;
+    }
+}
