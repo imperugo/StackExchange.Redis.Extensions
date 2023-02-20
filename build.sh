1b0e24c17a6ccedc3cbb05f88f9b123e0a14a047
imperugo <imperugo@gmail.com>
2023-02-20 10:22:03 +0100

    Wrong target framework on pipeline unit tests

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ find . -iname "bin" -o -iname "obj" | xargs rm -rf
 
 dotnet test -f net7.0 --verbosity quiet
 dotnet test -f net6.0 --verbosity quiet
-dotnet test -f netcoreapp3.1 --verbosity quiet
+dotnet test -f netcoreapp2.0 --verbosity quiet
